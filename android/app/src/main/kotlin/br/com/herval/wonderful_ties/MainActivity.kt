76e3567cc00115cfb7a37c937a0243ecbd1ffde5
HervalMata <hervalmataamparo@gmail.com>
2020-10-27 21:43:55 -0300

    Login e Cadastro

---
+++ android/app/src/main/kotlin/br/com/herval/wonderful_ties/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.herval.wonderful_ties
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:wonderful_ties/models/user_manager.dart';
+import 'package:wonderful_ties/screens/login/login_screen.dart';
+import 'package:wonderful_ties/screens/signup/signup_screen.dart';
+
+import 'screens/base/base_screen.dart';
+
+void main() {
+  runApp(MyApp());
+  
+  //Firestore.instance.collection('teste').add({'teste': 'teste'});
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => UserManager(),
+      lazy: false,
+      child: MaterialApp(
+        title: 'Laços da Cris',
+        theme: ThemeData(
+          primaryColor: const Color.fromARGB(255, 249, 69, 252),
+          scaffoldBackgroundColor: const Color.fromARGB(255, 249, 69, 252),
+          appBarTheme: const AppBarTheme(elevation: 0),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        initialRoute: '/base',
+        onGenerateRoute: (settings) {
+          switch(settings.name){
+            case '/login':
+              return MaterialPageRoute(
+                  builder: (_) => LoginScreen()
+              );
+            case '/signup':
+              return MaterialPageRoute(
+                  builder: (_) => SignUpScreen()
+              );
+            case '/base':
+            default:
+              return MaterialPageRoute(
+                  builder: (_) => BaseScreen()
+              );
+          }
+        },
+      ),
+    );
+  }
+}
