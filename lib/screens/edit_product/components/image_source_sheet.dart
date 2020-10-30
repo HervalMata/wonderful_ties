121ec1d6a57b4d541f5959e572ac2e260f63e644
HervalMata <hervalmataamparo@gmail.com>
2020-10-30 03:06:53 -0300

    Exibindo CupertinoModalPopup Camera/Galeria (iOS)

--- lib/screens/edit_product/components/image_source_sheet.dart
+++ lib/screens/edit_product/components/image_source_sheet.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ImageSourceSheet extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return BottomSheet(
+    if(Platform.isAndroid)
+      return BottomSheet(
         onClosing: (){},
         builder: (_) => Column(
           mainAxisSize: MainAxisSize.min,
@@ -24,5 +28,30 @@ class ImageSourceSheet extends StatelessWidget{
           ],
         )
     );
+    else
+      return CupertinoActionSheet(
+        title: const Text('Selecionar foto para o item'),
+        message: const Text('Escolha a origem da foto'),
+        cancelButton: CupertinoActionSheetAction(
+          onPressed: Navigator.of(context).pop,
+          child: const Text('Cancelar'),
+        ),
+        actions: <Widget> [
+          CupertinoActionSheetAction(
+            isDefaultAction: true,
+              onPressed: (){
+
+              },
+              child: const Text('Câmera')
+          ),
+          CupertinoActionSheetAction(
+              isDefaultAction: true,
+              onPressed: (){
+
+              },
+              child: const Text('Galeria')
+          )
+        ],
+      );
   }
 }
