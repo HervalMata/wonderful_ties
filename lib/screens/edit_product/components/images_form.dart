ec47cc6ae7655fe0e34b39ecbc5e9f147b4506a5
HervalMata <hervalmataamparo@gmail.com>
2020-10-30 02:56:45 -0300

    Exibindo BottomSheet Camera/Galeria (Android)

--- lib/screens/edit_product/components/images_form.dart
+++ lib/screens/edit_product/components/images_form.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 import 'package:wonderful_ties/models/product.dart';
+import 'package:wonderful_ties/screens/edit_product/components/image_source_sheet.dart';
 
 class ImagesForm extends StatelessWidget{
   final Product product;
@@ -17,7 +18,7 @@ class ImagesForm extends StatelessWidget{
           return AspectRatio(
               aspectRatio: 1,
             child: Carousel(
-              images: state.value.map((image){
+              images: state.value.map<Widget>((image){
                 return Stack(
                   fit: StackFit.expand,
                   children: <Widget> [
@@ -36,7 +37,22 @@ class ImagesForm extends StatelessWidget{
                     )
                   ],
                 );
-              }).toList(),
+              }).toList()..add(
+                Material(
+                  color: Colors.grey[100],
+                  child: IconButton(
+                    icon: Icon(Icons.add_a_photo),
+                    color: Theme.of(context).primaryColor,
+                    iconSize: 50,
+                    onPressed: (){
+                      showModalBottomSheet(
+                          context: context,
+                          builder: (_) => ImageSourceSheet()
+                      );
+                    },
+                  ),
+                )
+              ),
               dotSize: 4,
               dotSpacing: 15,
               dotBgColor: Colors.transparent,
