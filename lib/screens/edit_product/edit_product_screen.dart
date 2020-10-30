f41f298f528e2aa8f012205ffbdf361e1f9663aa
HervalMata <hervalmataamparo@gmail.com>
2020-10-30 18:42:03 -0300

    Editando o Titulo e Descricao e Corrigindo Preco Base

--- lib/screens/edit_product/edit_product_screen.dart
+++ lib/screens/edit_product/edit_product_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:wonderful_ties/models/product.dart';
 import 'package:wonderful_ties/screens/edit_product/components/images_form.dart';
@@ -9,6 +10,7 @@ class EditProductScreen extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
+    final primaryColor = Theme.of(context).primaryColor;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Editar Anuncio'),
@@ -19,14 +21,79 @@ class EditProductScreen extends StatelessWidget{
         child: ListView(
           children: <Widget> [
             ImagesForm(product),
-            RaisedButton(
-                onPressed: (){
-                  if(formKey.currentState.validate()){
-                    print('válido!!!');
-                  }
-                },
-              child: const Text('Salvar'),
-            ),
+            Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget> [
+                    TextFormField(
+                      initialValue: product.name,
+                      decoration: const InputDecoration(
+                        hintText: 'Titulo',
+                        border: InputBorder.none,
+                      ),
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600
+                      ),
+                      validator: (name){
+                        if(name.length < 6) return 'Titulo muito curto';
+                        return null;
+                      },
+                    ),
+                    Padding(
+                        padding: const EdgeInsets.only(top: 4),
+                        child: Text(
+                          'A partir de',
+                          style: TextStyle(
+                              fontSize: 13,
+                              color: Colors.grey[600],
+                          ),
+                        ),
+                    ),
+                    Text(
+                      'R\$ ...',
+                      style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: primaryColor,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 16),
+                      child: Text(
+                        'Descrição',
+                        style: TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.w600
+                        ),
+                      ),
+                    ),
+                    TextFormField(
+                      initialValue: product.description,
+                      style: TextStyle(
+                          fontSize: 16,
+                      ),
+                      decoration: const InputDecoration(
+                        hintText: 'Descrição',
+                        border: InputBorder.none,
+                      ),
+                      maxLines: null,
+                      validator: (desc){
+                        if(desc.length < 10) return 'Descrição muito curta';
+                        return null;
+                      },
+                    ),
+                    RaisedButton(
+                      onPressed: (){
+                        if(formKey.currentState.validate()){
+                          print('válido!!!');
+                        }
+                      },
+                      child: const Text('Salvar'),
+                    ),
+                  ],
+                ),
+            )
           ],
         ),
       ),
