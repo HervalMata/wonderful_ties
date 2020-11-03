a37ebb998112d6b627b8f91ed0bdf4d709f21981
HervalMata <hervalmataamparo@gmail.com>
2020-11-03 00:05:40 -0300

    Filtrando os Pedidos por Status

--- lib/models/admin_orders_manager.dart
+++ lib/models/admin_orders_manager.dart
@@ -7,8 +7,9 @@ import 'package:wonderful_ties/models/order.dart';
 import 'package:wonderful_ties/models/user.dart';
 
 class AdminOrdersManager extends ChangeNotifier{
-  List<Order> _orders = [];
+  final List<Order> _orders = [];
   User userFilter;
+  List<Status> statusFilter = [Status.preparing];
   final Firestore firestore = Firestore.instance;
   StreamSubscription _subscription;
 
@@ -25,7 +26,7 @@ class AdminOrdersManager extends ChangeNotifier{
     if(userFilter != null){
       output = output.where((o) => o.userId == userFilter.id).toList();
     }
-    return output;
+    return output.where((o) => statusFilter.contains(o.status)).toList();
   }
 
   void _listenToOrders() {
@@ -54,6 +55,15 @@ class AdminOrdersManager extends ChangeNotifier{
 
   void setUserFilter(User user){
     userFilter = user;
+    notifyListeners();
+  }
+
+  void setStatusFilter({Status status, bool enabled}){
+    if(enabled){
+      statusFilter.add(status);
+    } else {
+      statusFilter.remove(status);
+    }
     notifyListeners();
   }
 
