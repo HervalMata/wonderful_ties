5d02624ecf35d4709f8ae01805c8068f97a37ce5
HervalMata <hervalmataamparo@gmail.com>
2020-11-04 02:13:15 -0300

    Criando um Cartao Animado

--- lib/screens/checkout/checkout_screen.dart
+++ lib/screens/checkout/checkout_screen.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:wonderful_ties/common/price_card.dart';
 import 'package:wonderful_ties/models/cart_manager.dart';
 import 'package:wonderful_ties/models/checkout_manager.dart';
+import 'package:wonderful_ties/screens/checkout/components/credit_card_widget.dart';
 
 class CheckoutScreen extends StatelessWidget{
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
@@ -46,6 +47,7 @@ class CheckoutScreen extends StatelessWidget{
             }
             return ListView(
               children: <Widget> [
+                CreditCardWidget(),
                 PriceCard(
                   buttonText: 'Finalizar Pedido',
                   onPressd: (){
